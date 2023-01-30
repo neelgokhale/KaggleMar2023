771928d25a6ff282f452799a7ed40efd43549190
neelgokhale <neelg14@gmail.com>
2023-01-29 21:53:52 -0500

    init

--- code/.ipynb_checkpoints/constants-checkpoint.py
+++
@@ -1,8 +0,0 @@
-import os
-
-# file paths
-    
-ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-NB_DIR = os.path.join(ROOT_DIR, 'notebooks')
-DATA_DIR = os.path.join(ROOT_DIR, 'data')
-    

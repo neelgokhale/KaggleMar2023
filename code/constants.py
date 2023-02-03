f026f2107973d3081326813db109767e5fd2e880
neelgokhale <neelg14@gmail.com>
2023-02-03 10:24:27 -0500

    minor updates

--- code/constants.py
+++ code/constants.py
@@ -5,8 +5,9 @@ import os
 ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 NB_DIR = os.path.join(ROOT_DIR, 'notebooks')
 DATA_DIR = os.path.join(ROOT_DIR, 'data')
+OUTPUT_DIR = os.path.join(ROOT_DIR, 'output')
+
 TRAIN_PATH = os.path.join(DATA_DIR, 'train.csv')
 TEST_PATH = os.path.join(DATA_DIR, 'test.csv')
 CENSUS_PATH = os.path.join(DATA_DIR, 'census_starter.csv')
 MD_PATH = os.path.join(DATA_DIR, 'md-counties.csv')
-    
